bf50aea1c1a7114ddc1757c7af89a871d5f6e23c
Ankur Gupta <rukna1000@gmail.com>
2017-12-05 16:39:56 -0500

    Added comments for AdditiveExpression.java

--- src/AdditiveExpression.java
+++ src/AdditiveExpression.java
@@ -1,10 +1,25 @@
+/**
+ * Child of expression that represents an addition operator.
+ */
 public class AdditiveExpression extends CollapsibleExpression{
-    public Expression deepCopy(){
+	/**
+	 * Will return a copy of the Additive expression to use
+	 * for the GUI.
+	 * @return an copy of the {@link AdditiveExpression}
+	 */
+    public Expression deepCopy() {
         AdditiveExpression copy = new AdditiveExpression();
         return super.deepCopy(copy);
     }
+	
+	/**
+	 * Will generate a String representation for the the addition
+	 * operator, with the appropiate number of tabs to properly 
+	 * represent it's position in the expression.
+	 * @param indentLevel how many "levels" down the operator is
+	 * @return a String representating the addition operator.
+	 */
     public String convertToString(int indentLevel){
         return super.convertToString(indentLevel,"+");
     }
-
 }
