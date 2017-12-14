f4ddaa9586faf450d96529163c9586bcaeb5a44a
Bryt12 <brysontang@gmail.com>
2017-12-14 04:39:47 -0500

    Started adding comments!

--- src/ExpressionEditor.java
+++ src/ExpressionEditor.java
@@ -50,211 +50,216 @@ public class ExpressionEditor extends Application {
 			final double sceneX = event.getSceneX();
 			final double sceneY = event.getSceneY();
 
+
+			//If the mouse is clicked, look at the clicked location and check each node to see if it was clicked.
 			if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
-				ObservableList<Node> HChildren = ((HBox) currentFocus_).getChildren();
-
-				if(HChildren.size() == 1 && event.isDragDetect()){
-					return;
-				}
-
-				boolean found = false;
-				for (int i = 0; i < HChildren.size(); i++) {
-					final Node currentNode = HChildren.get(i);
-					if (currentNode instanceof HBox && !event.isDragDetect()) {
-						Point2D relativeClick = currentNode.sceneToLocal(sceneX, sceneY);
-						if (currentNode.contains(relativeClick.getX(), relativeClick.getY())) {
-							previousFocus = currentFocus_;
-							currentFocus_ = currentNode;
-
-							Point2D currentLocation = currentFocus_.localToScene(currentFocus_.getLayoutX(), currentFocus_.getLayoutY());
-
-							copyFocus_ = newLabel(((CopyAble) nodeMap.get(currentFocus_)).convertToStringFlat());
-							copyFocus_.setLayoutX(currentLocation.getX()-currentNode.getLayoutX());
-							copyFocus_.setLayoutY(currentLocation.getY()-((HBox) currentNode).getHeight()/2);
-
-							expressionPane.getChildren().add(copyFocus_);
-
-							if(currentFocus_ instanceof Label){
-								break;
-							}
-							((HBox) previousFocus).setBorder(Expression.NO_BORDER);
-							((HBox) currentNode).setBorder(Expression.RED_BORDER);
-							found = true;
-						}
-					}
-				}
-				if(!found) {
-					((HBox) currentFocus_).setBorder(Expression.NO_BORDER);
-					restart = true;
-				}
-
-                Expression focusedExpression = nodeMap.get(currentFocus_);
-                if(!currentFocus_.equals(rootExpression_.getNode())) {
-
-                    distances = new ArrayList<>();
-                    expressions = new ArrayList<>();
-
-					for (Expression e : AbstractCompoundExpression.generateAllPossibleTrees(
-                            (focusedExpression.getParent()).deepCopy(),
-                            focusedExpression.convertToString(0))) {
-						getWidthOfNode(e, this.currentFocus_);
-                        int minDistance = Integer.MAX_VALUE;
-                        for(int i = 0; i < distances.size(); i++){
-                            int localX = (int) expressions.get(0).getNode().sceneToLocal(sceneX, sceneY).getX();
-                            if(Math.abs(localX-distances.get(i)) < minDistance){
-                                minDistance = Math.abs(localX-distances.get(i));
-                                closesExpression = i;
-                            }
-                        }
-                    }
-                }
+				handleClick(event, sceneX, sceneY);
 			}
 			else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED && !restart) {
-			    int minDistance = Integer.MAX_VALUE;
-			    for(int i = 0; i < distances.size(); i++){
-					int localX = (int) expressions.get(0).getNode().sceneToLocal(sceneX, sceneY).getX();
-					if(Math.abs(localX-distances.get(i)) < minDistance){
-						minDistance = Math.abs(localX-distances.get(i));
-                        closesExpression = i;
-                    }
-                }
-				addToRoot(expressions.get(closesExpression), ((AbstractCompoundExpression)expressions.get(closesExpression)).getChildren().get(0));
-				LinkedList<Expression> children = ((AbstractCompoundExpression)rootExpression_).getChildren();
-				HBox hb = new HBox();
-
-				//type values
-				//1: Additive
-				//2: Multiplative
-				//3: Parenthetical
-				int type = 0;
-				if(rootExpression_ instanceof AdditiveExpression){
-					type = 1;
-				}else if(rootExpression_ instanceof MultiplicativeExpression){
-					type = 2;
-				}else if(rootExpression_ instanceof  ParentheticalExpression){
-					type = 3;
-				}else if(rootExpression_ instanceof LiteralExpression){
-					type = 4;
-					hb.getChildren().add(rootExpression_.getNode());
-				}
-
-				if(type != 4) {
-					for (int i = 0; i < children.size(); i++) {
-						if (type == 1 && hb.getChildren().size() != 0) {
-							hb.getChildren().add(newLabel("+"));
-						} else if (type == 2 && hb.getChildren().size() != 0) {
-							hb.getChildren().add(newLabel("*"));
-						} else if (type == 3) {
-							hb.getChildren().add(newLabel("("));
-						}
-						boolean containsFocus = checkForFocus(children.get(i));
-						if (containsFocus) {
-							HBox t = fixFocus(children.get(i));
-							hb.getChildren().add(t);
-						} else {
-							if (children.get(i).getNode().equals(currentFocus_)) {
-								//makeGray(chillin.get(i));
-							}
-							hb.getChildren().add(children.get(i).getNode());
-						}
-						if (type == 3) {
-							hb.getChildren().add(newLabel(")"));
-						}
-					}
-				}
-				expressionPane.getChildren().clear();
-				expressionPane.getChildren().add(hb);
-				expressionPane.getChildren().add(copyFocus_);
-
-				recolor(currentFocus_, Expression.GHOST_COLOR);
-
-				hb.setLayoutX(WINDOW_WIDTH / 2);
-				hb.setLayoutY(WINDOW_HEIGHT / 2);
-
-				if(copyFocus_ != rootExpression_.getNode()) {
-					copyFocus_.setTranslateX(copyFocus_.getTranslateX() + (sceneX - _lastX));
-					copyFocus_.setTranslateY(copyFocus_.getTranslateY() + (sceneY - _lastY));
-				}
+				handleDrag(event, sceneX, sceneY);
 			}
 			else if (event.getEventType() == MouseEvent.MOUSE_RELEASED) {
-			    recolor(currentFocus_, Color.BLACK);
-				copyFocus_ = new HBox();
-				//On release update the root expression to be the closes expression to the mouse.
-                if(!restart) {
-                    addToRoot(expressions.get(closesExpression), ((AbstractCompoundExpression) expressions.get(closesExpression)).getChildren().get(0));
-                }
-				LinkedList<Expression> children = ((AbstractCompoundExpression)rootExpression_).getChildren();
-				HBox hb = new HBox();
-
-				//type values
-				//1: Additive
-				//2: Multiplative
-				//3: Parenthetical
-				int type = 0;
-				if(rootExpression_ instanceof AdditiveExpression){
-					type = 1;
-				}else if(rootExpression_ instanceof MultiplicativeExpression){
-					type = 2;
-				}else if(rootExpression_ instanceof  ParentheticalExpression){
-					type = 3;
-				}else if(rootExpression_ instanceof LiteralExpression){
-					type = 4;
-					hb.getChildren().add(rootExpression_.getNode());
-				}
-				if(type != 4) {
-					for (int i = 0; i < children.size(); i++) {
-						if (type == 1 && hb.getChildren().size() != 0) {
-							hb.getChildren().add(newLabel("+"));
-						} else if (type == 2 && hb.getChildren().size() != 0) {
-							hb.getChildren().add(newLabel("*"));
-						} else if (type == 3) {
-							hb.getChildren().add(newLabel("("));
-						}
-						boolean containsFocus = checkForFocus(children.get(i));
-						if (containsFocus) {
-							HBox t = fixFocus(children.get(i));
-							hb.getChildren().add(t);
-						} else {
-							hb.getChildren().add(children.get(i).getNode());
-						}
-						if (type == 3) {
-							hb.getChildren().add(newLabel(")"));
-						}
-					}
-				}
-				if(restart){
-                    try {
-                        // Success! Add the expression's Node to the expressionPane
-                        final Expression expression = expressionParser.parse(hbToString(hb), true);
-
-                        expressionPane.getChildren().clear();
-                        expressionPane.getChildren().add(expression.getNode());
-
-                        expression.getNode().setLayoutX(WINDOW_WIDTH / 2);
-                        expression.getNode().setLayoutY(WINDOW_HEIGHT / 2);
-
-                        rootExpression_ = ((CompoundExpression) expression);
-                        currentFocus_ = rootExpression_.getNode();
-                        expression.flatten();
-                        nodeMap = generateMap(expression);
-                        restart = false;
-                    } catch (ExpressionParseException epe) {
-                    }
-                }else {
-                    expressionPane.getChildren().clear();
-                    expressionPane.getChildren().add(hb);
-
-                    hb.setLayoutX(WINDOW_WIDTH / 2);
-                    hb.setLayoutY(WINDOW_HEIGHT / 2);
-                }
-
-                closesExpression = 0;
+				handleRelease(event, sceneX, sceneY);
 			}
 			_lastX = sceneX;
 			_lastY = sceneY;
 
 		}
 
+
+		private void handleClick(MouseEvent event, double sceneX, double sceneY){
+			//Get the children of the current focus to be checked
+			ObservableList<Node> HChildren = ((HBox) currentFocus_).getChildren();
+
+			//If the focus is a single variable then don't bother checking for
+			if(HChildren.size() == 1 && event.isDragDetect()){
+				return;
+			}
+
+			boolean found = false;
+			//Go through each child of the current focus
+			for (int i = 0; i < HChildren.size(); i++) {
+				final Node currentNode = HChildren.get(i);
+				//If the current node is a HBox (so not a Label) check it
+				if (currentNode instanceof HBox && !event.isDragDetect()) {
+					//Turn the click into terms of the current node
+					Point2D relativeClick = currentNode.sceneToLocal(sceneX, sceneY);
+					if (currentNode.contains(relativeClick.getX(), relativeClick.getY())) {
+						//Change the focus
+						previousFocus = currentFocus_;
+						currentFocus_ = currentNode;
+
+						Point2D currentLocation = currentFocus_.localToScene(currentFocus_.getLayoutX(), currentFocus_.getLayoutY());
+						//Make Label to follow around mouse
+						copyFocus_ = newLabel(((CopyAble) nodeMap.get(currentFocus_)).convertToStringFlat());
+						copyFocus_.setLayoutX(currentLocation.getX()-currentNode.getLayoutX());
+						copyFocus_.setLayoutY(currentLocation.getY()-((HBox) currentNode).getHeight()/2);
+
+						expressionPane.getChildren().add(copyFocus_);
+
+						if(currentFocus_ instanceof Label){
+							break;
+						}
+						//Deselect previous focus
+						((HBox) previousFocus).setBorder(Expression.NO_BORDER);
+						((HBox) currentNode).setBorder(Expression.RED_BORDER);
+						found = true;
+					}
+				}
+			}
+			//If it wasn't found then the user didn't click on anything so restart focus
+			if(!found) {
+				((HBox) currentFocus_).setBorder(Expression.NO_BORDER);
+				restart = true;
+			}
+			//As long as something was selected begin the process generating all the possible trees
+			Expression focusedExpression = nodeMap.get(currentFocus_);
+			if(!currentFocus_.equals(rootExpression_.getNode())) {
+
+				distances.clear();
+				expressions.clear();
+
+				for (Expression e : AbstractCompoundExpression.generateAllPossibleTrees((focusedExpression.getParent()).deepCopy(), focusedExpression.convertToString(0))) {
+					//Calls function that calculates the x position of each possibility
+					getWidthOfNode(e, this.currentFocus_);
+					//After gets the current possibility by checking which is the closest
+					calculateClosestPosition(sceneX, sceneY);
+				}
+			}
+		}
+
+		private void handleDrag(MouseEvent event, double sceneX, double sceneY){
+			//Calculate the closest position
+			calculateClosestPosition(sceneX, sceneY);
+			//Get the closest orientation then append it to the root node in order to create the full tree
+			addToRoot(expressions.get(closesExpression), ((AbstractCompoundExpression)expressions.get(closesExpression)).getChildren().get(0));
+			//Generate the next tree
+			HBox hb = generateHBox();
+			//Display the new tree
+			expressionPane.getChildren().clear();
+			expressionPane.getChildren().add(hb);
+			expressionPane.getChildren().add(copyFocus_);
+
+			recolor(currentFocus_, Expression.GHOST_COLOR);
+
+			hb.setLayoutX(WINDOW_WIDTH / 2);
+			hb.setLayoutY(WINDOW_HEIGHT / 2);
+			//If the root is not the current focus move the current focus in the direction of the mouse
+			if(copyFocus_ != rootExpression_.getNode()) {
+				copyFocus_.setTranslateX(copyFocus_.getTranslateX() + (sceneX - _lastX));
+				copyFocus_.setTranslateY(copyFocus_.getTranslateY() + (sceneY - _lastY));
+			}
+		}
+
+		private void handleRelease(MouseEvent event, double sceneX, double sceneY) {
+			recolor(currentFocus_, Color.BLACK);
+			copyFocus_ = new HBox();
+			//On release update the root expression to be the closes expression to the mouse, but only do this if
+			//not de-selecting the focus
+			if(!restart) {
+				addToRoot(expressions.get(closesExpression), ((AbstractCompoundExpression) expressions.get(closesExpression)).getChildren().get(0));
+			}
+
+			HBox hb = generateHBox();
+			//If de-selecting the focus (restart == true) rebuild the root expression out of the current
+			//expression on the screen
+			if(restart){
+				try {
+					final Expression expression = expressionParser.parse(hbToString(hb), true);
+
+					expressionPane.getChildren().clear();
+					expressionPane.getChildren().add(expression.getNode());
+
+					expression.getNode().setLayoutX(WINDOW_WIDTH / 2);
+					expression.getNode().setLayoutY(WINDOW_HEIGHT / 2);
+
+					rootExpression_ = ((CompoundExpression) expression);
+					currentFocus_ = rootExpression_.getNode();
+					expression.flatten();
+					nodeMap = generateMap(expression);
+					restart = false;
+				} catch (ExpressionParseException epe) {
+				}
+			}else {
+				//If not just update the pane
+				expressionPane.getChildren().clear();
+				expressionPane.getChildren().add(hb);
+
+				hb.setLayoutX(WINDOW_WIDTH / 2);
+				hb.setLayoutY(WINDOW_HEIGHT / 2);
+			}
+
+			closesExpression = 0;
+		}
+
+		private HBox generateHBox(){
+			HBox hb = new HBox();
+			//Since rootExpression_.getNode() returns a list of nodes not in order, get the children
+			//which is a linked list
+			LinkedList<Expression> children = ((AbstractCompoundExpression)rootExpression_).getChildren();
+
+			//type values
+			//1: Additive
+			//2: Multiplative
+			//3: Parenthetical
+			int type = 0;
+			//Since only the children of the rootExpression_ are preserved in the children linked list
+			//the type of expression must be figured out.
+			if(rootExpression_ instanceof AdditiveExpression){
+				type = 1;
+			}else if(rootExpression_ instanceof MultiplicativeExpression){
+				type = 2;
+			}else if(rootExpression_ instanceof  ParentheticalExpression){
+				type = 3;
+			}else if(rootExpression_ instanceof LiteralExpression){
+				type = 4;
+				hb.getChildren().add(rootExpression_.getNode());
+			}
+
+			if(type != 4) {
+				//Go through the children
+				for (int i = 0; i < children.size(); i++) {
+					//Check to see if any children are added, if so add a sign between them
+					if (type == 1 && hb.getChildren().size() != 0) {
+						hb.getChildren().add(newLabel("+"));
+					} else if (type == 2 && hb.getChildren().size() != 0) {
+						hb.getChildren().add(newLabel("*"));
+					} else if (type == 3) {
+						hb.getChildren().add(newLabel("("));
+					}
+
+					//Check to see if branch contains focus because focus is the only thing that can change
+					//so a next HBox must be made for it
+					boolean containsFocus = checkForFocus(children.get(i));
+					if (containsFocus) {
+						//If the node contains the focus then "fix" the focus by manually creating an HBox
+						HBox t = fixFocus(children.get(i));
+						hb.getChildren().add(t);
+					} else {
+						//If not just add the node since the order is preserved and not altered by user
+						hb.getChildren().add(children.get(i).getNode());
+					}
+					if (type == 3) {
+						hb.getChildren().add(newLabel(")"));
+					}
+				}
+			}
+			return hb;
+		}
+
+		private void calculateClosestPosition(double sceneX, double sceneY){
+			int minDistance = Integer.MAX_VALUE;
+			//Goes through each distance and sets closestExperssion to which ever one is the closest
+			for(int i = 0; i < distances.size(); i++){
+				int localX = (int) expressions.get(0).getNode().sceneToLocal(sceneX, sceneY).getX();
+				if(Math.abs(localX-distances.get(i)) < minDistance){
+					minDistance = Math.abs(localX-distances.get(i));
+					closesExpression = i;
+				}
+			}
+		}
+
+		//Recusively goes down HBox until reaches Label then appends all the values together
         private String hbToString(HBox h){
             String result = "";
 
@@ -270,6 +275,7 @@ public class ExpressionEditor extends Application {
             return result;
         }
 
+		//Creates a new HBox in the direction of the focus
 		private HBox fixFocus(Expression e){
 			HBox hb = new HBox();
 			int type = 0;
@@ -326,6 +332,7 @@ public class ExpressionEditor extends Application {
 			return false;
 		}
 
+		//Function to find node and replace it with the new order
 		private void addToRoot(Expression parentE, Expression e){
 			addToRootHelper(parentE, e, ((AbstractCompoundExpression) rootExpression_).getChildren(), rootExpression_);
 		}
